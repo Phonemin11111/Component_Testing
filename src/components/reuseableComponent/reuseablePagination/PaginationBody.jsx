1e6eafd9a1c7eeca8f33bdaa81820a820373a1d1
Phonemin11111 <phonemin01@gmail.com>
2025-01-31 17:27:09 -0800

    troubleshot

--- src/components/reuseableComponent/reuseablePagination/PaginationBody.jsx
+++ src/components/reuseableComponent/reuseablePagination/PaginationBody.jsx
@@ -96,7 +96,7 @@ const PaginationBody = ({ totalPages, currentPage, setCurrentPage }) => {
 
                 {dropdownType === page && (
                   <ul
-                    className={` absolute z-10 bg-white border border-gray-500 rounded shadow-md py-1 max-h-32 overflow-auto w-[32px] text-center`}
+                    className={`bottom-full ml-[-2px] absolute z-10 bg-white border border-gray-500 rounded shadow-md max-h-32 overflow-auto px-1.5 text-center`}
                   >
                     {getDropdownPages(page).map((dropdownPage) => (
                       <li key={dropdownPage}>
